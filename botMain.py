b2341031b541597f48edc4dcf4fed045ec642682
Olli Helminen <olli.helminen@optofidelity.com>
2019-01-31 21:12:36 +0200

    Build more supply and SCVs

--- botMain.py
+++ botMain.py
@@ -7,8 +7,9 @@ class WinterBot(sc2.BotAI):
     Bot for SC2 made by Winterburn
     """
     async def on_step(self, iteration):
-        if iteration == 0:
-            for worker in self.workers:
+        cc = self.units(constants.UnitTypeId.COMMANDCENTER)
+        cc = cc.first
+        for worker in self.workers:
                 await self.do(worker.attack(self.enemy_start_locations[0]))
 
 run_game(maps.get("(2)AcidPlantLE"), [
