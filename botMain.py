60a2a206ed064a00ee7af9d0e844465e9f25ed26
Olli Helminen <olli.helminen@optofidelity.com>
2019-01-31 11:13:18 +0200

    Renamed bot

--- botMain.py
+++ botMain.py
@@ -2,13 +2,16 @@ import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 
-class WorkerRushBot(sc2.BotAI):
+class WinterBot(sc2.BotAI):
+    """
+    Bot for SC2 made by Winterburn
+    """
     async def on_step(self, iteration):
         if iteration == 0:
             for worker in self.workers:
                 await self.do(worker.attack(self.enemy_start_locations[0]))
 
 run_game(maps.get("(2)AcidPlantLE"), [
-    Bot(Race.Zerg, WorkerRushBot()),
+    Bot(Race.Zerg, WinterBot()),
     Computer(Race.Protoss, Difficulty.Medium)
 ], realtime=True)
