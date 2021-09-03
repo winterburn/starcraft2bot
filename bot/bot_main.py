77d5e3902788026de3ee9dd750f06a6c64e980a9
Olli <opadele@gmail.com>
2021-09-04 00:40:00 +0300

    Add some marine training.

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -1,4 +1,5 @@
 """Mainfile for the starcraft bot"""
+from collections import defaultdict
 import sc2
 from sc2 import constants
 
@@ -15,6 +16,9 @@ class WinterBot(sc2.BotAI):
         for worker in self.workers:
             if worker.is_idle:
                 worker.gather(self.mineral_field.closest_to(self.com_cent))
+
+        await self.military_buildings()
+        await self.train_marines()
         await self.saturate_mining()
         await self.build_supply()
 
@@ -32,3 +36,34 @@ class WinterBot(sc2.BotAI):
         if self.com_cent.surplus_harvesters < 0:
             if self.can_afford(constants.UnitTypeId.SCV) and self.com_cent.is_idle:
                 self.com_cent.train(constants.UnitTypeId.SCV)
+
+    async def military_buildings(self):
+        """Check if there is missing military building and build it"""
+        if not self.get_building_count().get(constants.UnitTypeId.BARRACKS):
+            if self.can_afford(constants.UnitTypeId.BARRACKS):
+                await self.build(constants.UnitTypeId.BARRACKS, near=self.com_cent)
+
+    def get_building_count(self):
+        """Count the buildings to dict"""
+        count = defaultdict(int)
+        for structure in self.structures:
+            count[structure.type_id] += 1
+        return count
+
+    def get_military_count(self):
+        """Count the military units"""
+        count = defaultdict(int)
+        unit_list = [constants.UnitTypeId.MARINE]
+        for unit in unit_list:
+            count[unit] = len(self.units.filter(lambda u: u.type_id == unit))
+        return count
+
+    async def train_marines(self):
+        """Train marines units"""
+        barracks = self.structures.filter(lambda u: u.type_id == constants.UnitTypeId.BARRACKS)
+
+        if barracks:
+            marine_count = self.get_military_count().get(constants.UnitTypeId.MARINE)
+            if marine_count < 20 and self.can_afford(constants.UnitTypeId.MARINE):
+                if barracks[0].is_idle:
+                    barracks[0].train(constants.UnitTypeId.MARINE)
