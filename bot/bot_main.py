f4a959777690a65cf770a86fddd3b4e4cd095faa
Olli <opadele@gmail.com>
2021-09-04 12:52:07 +0300

    improving supply depot building. Only one at time

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -1,7 +1,7 @@
 """Mainfile for the starcraft bot"""
 from collections import defaultdict
 import sc2
-from sc2 import constants
+from sc2.constants import UnitTypeId
 
 
 class WinterBot(sc2.BotAI):
@@ -24,24 +24,24 @@ class WinterBot(sc2.BotAI):
 
     async def build_supply(self):
         """build supply if there is less than 1 supply left"""
-        if self.supply_left <= 1 and not self.units.filter(
-                lambda u: u.type_id == constants.UnitTypeId.SUPPLYDEPOT and
-                not u.is_ready):
-            pos = await self.find_placement(constants.UnitTypeId.SUPPLYDEPOT,
-                                            self.com_cent.position)
-            await self.build(constants.UnitTypeId.SUPPLYDEPOT, pos)
+        if self.supply_left <= 1:
+            # check that we have no supply depots already building
+            if not self.already_pending(UnitTypeId.SUPPLYDEPOT):
+                pos = await self.find_placement(UnitTypeId.SUPPLYDEPOT,
+                                                self.com_cent.position)
+                await self.build(UnitTypeId.SUPPLYDEPOT, pos)
 
     async def saturate_mining(self):
         """Saturate the mining of minerals"""
         if self.com_cent.surplus_harvesters < 0:
-            if self.can_afford(constants.UnitTypeId.SCV) and self.com_cent.is_idle:
-                self.com_cent.train(constants.UnitTypeId.SCV)
+            if self.can_afford(UnitTypeId.SCV) and self.com_cent.is_idle:
+                self.com_cent.train(UnitTypeId.SCV)
 
     async def military_buildings(self):
         """Check if there is missing military building and build it"""
-        if not self.get_building_count().get(constants.UnitTypeId.BARRACKS):
-            if self.can_afford(constants.UnitTypeId.BARRACKS):
-                await self.build(constants.UnitTypeId.BARRACKS, near=self.com_cent)
+        if not self.get_building_count().get(UnitTypeId.BARRACKS):
+            if self.can_afford(UnitTypeId.BARRACKS):
+                await self.build(UnitTypeId.BARRACKS, near=self.com_cent)
 
     def get_building_count(self):
         """Count the buildings to dict"""
@@ -53,17 +53,17 @@ class WinterBot(sc2.BotAI):
     def get_military_count(self):
         """Count the military units"""
         count = defaultdict(int)
-        unit_list = [constants.UnitTypeId.MARINE]
+        unit_list = [UnitTypeId.MARINE]
         for unit in unit_list:
             count[unit] = len(self.units.filter(lambda u: u.type_id == unit))
         return count
 
     async def train_marines(self):
         """Train marines units"""
-        barracks = self.structures.filter(lambda u: u.type_id == constants.UnitTypeId.BARRACKS)
+        barracks = self.structures.filter(lambda u: u.type_id == UnitTypeId.BARRACKS)
 
         if barracks:
-            marine_count = self.get_military_count().get(constants.UnitTypeId.MARINE)
-            if marine_count < 20 and self.can_afford(constants.UnitTypeId.MARINE):
+            marine_count = self.get_military_count().get(UnitTypeId.MARINE)
+            if marine_count < 20 and self.can_afford(UnitTypeId.MARINE):
                 if barracks[0].is_idle:
-                    barracks[0].train(constants.UnitTypeId.MARINE)
+                    barracks[0].train(UnitTypeId.MARINE)
